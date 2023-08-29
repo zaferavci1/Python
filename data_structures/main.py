c5e5258163e05e1e7137f93d67003400019d09e4
Zafer Avcı <93323232+zaferavci1@users.noreply.github.com>
2023-08-29 13:19:48 +0300

    Veri yapılarına giriş

---
+++ data_structures/main.py
@@ -0,0 +1,57 @@
+# Veri Yapıları(Data Structures)
+
+## Veri yapılarına giriş ve Hızlı Özet:
+### Sayılar integer:
+a=46
+type(a)
+
+### Sayılar float:
+a=10.3
+type(a)
+
+### Sayılar complex:
+x=2j+1
+type(x)
+
+### Sayılar string:
+a="zafer"
+type(a)
+
+### Sayılar bool:
+a=True
+type(a)
+
+### Sayılar liste:
+x = ["sdad", "adfasf", "zafer"]
+
+### Sayılar sözlük:
+x = {"zafer": "zafer", "age": 21}
+# sözlükler key:value tarzında oluşturulur.
+type(x)
+
+### Sayılar tuple:
+x = ("adsd", "zafer", "merhaba")
+type(x)
+
+### Sayılar set:
+x={"merhaba", "herkese"}
+# sözlüklerden farklı olarak : key value yok
+type(x)
+
+# Not: Liste, tuple, set ve dictionary veri yapıları aynı zamanda Python Collections (Arrays) olarak da ifade edilir.
+
+## Sayılar (Numbers): int, float, complex
+
+a = 5
+b = 10.5
+
+a * 3  # pep8 kanunları python yazanlar arasında uygulanan kuralları içerir.
+a / 7
+a * b / 10
+a ** 10
+
+# Tipleri değiştirme
+
+int(b)
+float(a)
+int(a * b / 10)
